11be7c5ae579579fa65f261f7064d7c6a70189d3
Vijay <sccr247@gmail.com>
2023-11-26 14:39:04 -0500

    update

--- main.py
+++ main.py
@@ -6,18 +6,23 @@ import cohere
 co = cohere.Client('rGjz0KNIMSReCgEyzpEUDQpYzxSoXb85RjjdyAel')
 from pytube import YouTube
 import json
+import webbrowser
 
 
+globalvideomap= {}
+documents = []
+urls = []
+
+def storeURLS(url):
+    playlist = Playlist(url)
+    for url in playlist:
+        urls.append(url)
 
 
 def YoutubeParse(url):
     # Retrieve URLs of videos from playlist
     playlist = Playlist(url)
     print('Number Of Videos In playlist: %s' % len(playlist.video_urls))
-
-    urls = []
-    for url in playlist:
-        urls.append(url)
 
     # print(urls)
 
@@ -28,7 +33,7 @@ def YoutubeParse(url):
     uid = 0
 
 
-    documents = []
+    
     for youtube_url in urls:
         match = re.search(pattern, youtube_url)
 
@@ -49,12 +54,13 @@ def YoutubeParse(url):
         text=transcript,
         )
         video_idx[uid] = (youtube_url, transcript, response.summary)
-        uid += 1
-
+        globalvideomap[uid] = youtube_url
         document = {
             'title: ': uid,
             'snippet': response.summary,
         }
+        uid += 1
+
         documents.append(document)
 
     # print(video_idx)
@@ -71,10 +77,48 @@ def YoutubeParse(url):
         file.write(json_data)
 
 
+
+def process(response):
+    citation_docs = response.citations
+    # print(citation_docs)
+    indexes_array = [entry['document_ids'][0] for entry in citation_docs]
+    print("INDEX OF VIDEO")
+    numbers_after_underscore = [int(entry.split('_')[1].split(':')[0]) if '_' in entry else None for entry in indexes_array]
+    print(numbers_after_underscore[0])
+    webbrowser.open(urls[numbers_after_underscore[0]])
+
+def openVid():
+    co = cohere.Client('rGjz0KNIMSReCgEyzpEUDQpYzxSoXb85RjjdyAel')
+    with open('./documents_auto.json', 'r') as file:
+        documents = json.load(file)
+    response = co.chat(
+    message= "what is a deadlock",
+    documents=documents,
+        prompt_truncation= "AUTO"
+    )
+    print(response)
+
+    # print("The response is ")
+
+    # print(citation_docs)
+    # indexes_array = [entry['document_ids'][0] for entry in citation_docs]
+    # val_array = [int(entry[-1]) for entry in indexes_array]
+    # print(val_array)
+
+    process(response)
+
 url = input("Enter Youtube playlist URL: ")
 
-YoutubeParse(url)
+# YoutubeParse(url)
+storeURLS(url)
 
 print("Output added to documents.json")
+
+print("Generating Response...")
+
+openVid()
+
+
+
 
 
