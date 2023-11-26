a97c619d9c0ba69faaae8160b5fd39365d38a9c1
katarinamakivic <kaya.makivic@gmail.com>
2023-11-26 12:28:50 -0500

    get all video ids in a playlist and transcribe

--- youtube_transcript.py
+++ youtube_transcript.py
@@ -1,18 +1,48 @@
-# importing the module
+import re
+from pytube import Playlist
 from youtube_transcript_api import YouTubeTranscriptApi
 
-# retrieve the available transcripts
-transcript_list = YouTubeTranscriptApi.list_transcripts('WDAnJpOuhI8&')
 
-# using the srt variable with the list of dictionaries
-# obtained by the .get_transcript() function
-srt = YouTubeTranscriptApi.get_transcript("WDAnJpOuhI8&")
+# URL of the YouTube playlist
+URL_PLAYLIST = "https://www.youtube.com/playlist?list=PLEoM_i-3sen_w5IYh0d5xtnpLHJeeO8l5"
 
-# creating or overwriting a file "subtitles.txt" with
-# the info inside the context manager
-with open("subtitles.txt", "w") as f:
+# Retrieve URLs of videos from playlist
+playlist = Playlist(URL_PLAYLIST)
+print('Number Of Videos In playlist: %s' % len(playlist.video_urls))
 
-        # iterating through each element of list srt
-    for i in srt:
-        # writing each element of srt on a new line
-        f.write("{}\n".format(i["text"]))
+urls = []
+for url in playlist:
+    urls.append(url)
+
+# print(urls)
+
+video_ids = []
+
+pattern = r'(?<=v=)[\w-]+'
+
+for youtube_url in urls:
+    # Find the video ID using regex
+    match = re.search(pattern, youtube_url)
+
+    if match:
+        video_id = match.group()
+        video_ids.append(video_id)
+        # print("Video ID:", video_id)
+    else:
+        print("Video ID not found.")
+
+# print(video_ids)
+
+for id in video_ids:
+    # retrieve the available transcripts
+    transcript_list = YouTubeTranscriptApi.list_transcripts(id)
+
+    # using the srt variable with the list of dictionaries
+    # obtained by the .get_transcript() function
+    srt = YouTubeTranscriptApi.get_transcript(id)
+
+    transcripts = {}
+    transcript = '\n'.join(i["text"] for i in srt)
+    transcripts[id] = transcript
+
+# print(transcripts)
