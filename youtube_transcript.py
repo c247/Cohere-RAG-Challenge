f6d7db3d1f6bbd9d618d98675f63810f813f4b42
Maryam Afshar <mary1afshar@gmail.com>
2023-11-26 13:17:57 -0500

    Documents.json file

--- youtube_transcript.py
+++ youtube_transcript.py
@@ -7,89 +7,64 @@ from pytube import YouTube
 import json
 
 # URL of the YouTube playlist
-URL_PLAYLIST = "https://www.youtube.com/playlist?list=PLEoM_i-3sen_w5IYh0d5xtnpLHJeeO8l5"
+# URL_PLAYLIST = "https://www.youtube.com/playlist?list=PLEoM_i-3sen_w5IYh0d5xtnpLHJeeO8l5"
 
-# Retrieve URLs of videos from playlist
-playlist = Playlist(URL_PLAYLIST)
-print('Number Of Videos In playlist: %s' % len(playlist.video_urls))
+def YoutubeParse(url):
+    # Retrieve URLs of videos from playlist
+    playlist = Playlist(url)
+    print('Number Of Videos In playlist: %s' % len(playlist.video_urls))
 
-urls = []
-for url in playlist:
-    urls.append(url)
+    urls = []
+    for url in playlist:
+        urls.append(url)
 
-# print(urls)
-
-
-pattern = r'(?<=v=)[\w-]+'
-
-video_idx = {}
-uid = 0
+    # print(urls)
 
 
-documents = []
-for youtube_url in urls:
-    match = re.search(pattern, youtube_url)
+    pattern = r'(?<=v=)[\w-]+'
 
-    if match:
-        video_id = match.group()
-    else:
-        print("Video ID not found.")
-
-    # retrieve the available transcripts
-    transcript_list = YouTubeTranscriptApi.list_transcripts(video_id)
-
-    # using the srt variable with the list of dictionaries
-    # obtained by the .get_transcript() function
-    srt = YouTubeTranscriptApi.get_transcript(video_id)
-
-    transcript = '\n'.join(i["text"] for i in srt)
-    response = co.summarize(
-    text=transcript,
-    )
-    video_idx[uid] = (youtube_url, transcript, response.summary)
-    uid += 1
-
-    document = {
-        'title: ': uid,
-        'snippet': response.summary,
-    }
-    documents.append(document)
-
-# print(video_idx)
+    video_idx = {}
+    uid = 0
 
 
-# documents = []
-# # cohere to summarize
-# for file in video_idx:
-#     url, transcript = video_idx[file]
+    documents = []
+    for youtube_url in urls:
+        match = re.search(pattern, youtube_url)
 
-#     response = co.summarize(
-#     text=transcript,
-#     )
+        if match:
+            video_id = match.group()
+        else:
+            print("Video ID not found.")
 
-#     # Create a YouTube object
-#     yt = YouTube(url)
+        # retrieve the available transcripts
+        transcript_list = YouTubeTranscriptApi.list_transcripts(video_id)
 
-#     # Get the title of the video
-#     video_title = yt.title
+        # using the srt variable with the list of dictionaries
+        # obtained by the .get_transcript() function
+        srt = YouTubeTranscriptApi.get_transcript(video_id)
 
-#     document = {
-#         'Title: ': video_title,
-#         'Summary': response.summary,
-#     }
+        transcript = '\n'.join(i["text"] for i in srt)
+        response = co.summarize(
+        text=transcript,
+        )
+        video_idx[uid] = (youtube_url, transcript, response.summary)
+        uid += 1
 
-#     print(document)
-#     documents.append(document)
+        document = {
+            'title: ': uid,
+            'snippet': response.summary,
+        }
+        documents.append(document)
 
-#     # get title3
+    # print(video_idx)
 
-# Convert documents list to JSON format
-json_data = json.dumps(documents, indent=2)  # Convert list of dictionaries to JSON string
+    # Convert documents list to JSON format
+    json_data = json.dumps(documents, indent=2)  # Convert list of dictionaries to JSON string
 
-# Print the JSON-formatted data
-print(json_data)
+    # Print the JSON-formatted data
+    print(json_data)
 
-# Write JSON data to a file
-output_file = 'documents.json'  
-with open(output_file, 'w') as file:
-    file.write(json_data)
+    # Write JSON data to a file
+    output_file = 'documents.json'  
+    with open(output_file, 'w') as file:
+        file.write(json_data)
