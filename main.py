0eba15a161cf41dd47bf61efcaa76b20a590ccac
Vijay <sccr247@gmail.com>
2023-12-04 20:09:35 -0500

    try

--- main.py
+++ main.py
@@ -12,7 +12,7 @@ import streamlit as st
 from dotenv import load_dotenv
 import os
 from openai import OpenAI
-
+from streamlit_lottie import st_lottie
 
 
 
@@ -35,53 +35,15 @@ def storeURLS(url):
     for url in playlist:
         urls.append(url)
 
-def Transcript(url):
-    pass
-    # Retrieve URLs of videos from playlist
-    # playlist = Playlist(url)
-    
-
-
-    # pattern = r'(?<=v=)[\w-]+'
-
-    # video_idx = {}
-    # uid = 0
-
-
-    
-    # for youtube_url in urls:
-    #     match = re.search(pattern, youtube_url)
-
-    #     if match:
-    #         video_id = match.group()
-    #     else:
-    #         print("Video ID not found.")
-
-
-    #     # using the srt variable with the list of dictionaries
-    #     # obtained by the .get_transcript() function
-    #     srt = YouTubeTranscriptApi.get_transcript(video_id)
-
-    #     transcript = '\n'.join(i["text"] for i in srt)
-    #     transcripts.append(transcript)
-
 
 
 def YoutubeParse(url):
     # Retrieve URLs of videos from playlist
-    playlist = Playlist(url)
-    # print('Number Of Videos In playlist: %s' % len(playlist.video_urls))
-
-    # print(urls)
-
-
     pattern = r'(?<=v=)[\w-]+'
 
     video_idx = {}
     uid = 0
 
-
-    
     for youtube_url in urls:
         match = re.search(pattern, youtube_url)
 
@@ -135,25 +97,7 @@ def process(response):
     global transcriptSelected 
     transcriptSelected = numbers_after_underscore[0]
     return urls[numbers_after_underscore[0]]
-
-def processToGetDocIndex(response):
-    citation_docs = response.citations
-    # print(citation_docs)
-    indexes_array = [entry['document_ids'][0] for entry in citation_docs]
-    # print("INDEX OF VIDEO")
-    numbers_after_underscore = [int(entry.split('_')[1].split(':')[0]) if '_' in entry else None for entry in indexes_array]
-    # print(numbers_after_underscore[0])
-    return numbers_after_underscore[0]
-
-# def getdocindex(response):
-#     citation_docs = response.citations
-#     # print(citation_docs)
-#     indexes_array = [entry['document_ids'][0] for entry in citation_docs]
-#     print("INDEX OF VIDEO")
-#     numbers_after_underscore = [int(entry.split('_')[1].split(':')[0]) if '_' in entry else None for entry in indexes_array]
-#     print(numbers_after_underscore[0]-1)
-
-    
+ 
 
 def openVid():
     global usermsg
@@ -185,81 +129,10 @@ def finalOpenTime(time, url):
     urltime = url + "&t=" + str(time)
     global finalURL
     finalURL = urltime
-    # webbrowser.open(urltime)
 
-
-def split_transcript(transcript, max_words=300):
-    print(transcript)
-    words = transcript.split()
-    grouped_transcripts = []
-
-    current_group = []
-    current_word_count = 0
-
-    for word in words:
-        current_group.append(word)
-        current_word_count += 1
-
-        if current_word_count >= max_words:
-            grouped_transcripts.append(" ".join(current_group))
-            current_group = []
-            current_word_count = 0
-
-    if current_group:
-        grouped_transcripts.append(" ".join(current_group))
-
-    return grouped_transcripts
-
-
-def create_json_array(grouped_transcripts):
-    json_array = []
-
-    for idx, group in enumerate(grouped_transcripts):
-        # You can replace this with your actual logic to generate summaries
-        response_summary = f"Summary for part {idx + 1}"
-
-        json_object = {
-            "title": str(idx),
-            "snippet": response_summary,
-        }
-
-        json_array.append(json_object)
-
-    return json_array
 
 
 def find_question_position_basic(transcript, question, video_length):
-    # grouped = split_transcript(question)
-    # totalDocuments = len(grouped)
-    # jsonDocuments = create_json_array(grouped)
-    # global usermsg
-    # response = co.chat(
-    #     message=usermsg ,
-    #     documents=jsonDocuments,
-    #     prompt_truncation= "AUTO"
-    # )
-
-    # indexOfDocument = processToGetDocIndex(response)
-    # return indexOfDocument/totalDocuments
-    
-    # print(transcript)
-    # # Convert both transcript and question to lowercase for case-insensitive comparison
-    # transcript_lower = transcript.lower()
-    # question_lower = question.lower()
-
-    # # Find the position of the question in the transcript
-    # question_start = transcript_lower.find(question_lower)
-
-    # # If the question is not found, return -1
-    # if question_start == -1:
-    #     return -1
-
-    # # Calculate the percentage position based on the character offsets
-    # percentage_position = (question_start / len(transcript_lower))
-    # print("percent")
-    # print(percentage_position)
-    # return percentage_position
-
 
     client = OpenAI(api_key=st.secrets["openaikey"],)
     # client = OpenAI(api_key=os.getenv("API_KEY"),)
@@ -286,8 +159,6 @@ def find_question_position_basic(transcript, question, video_length):
 def action(url, msg):
     storeURLS(url)
     YoutubeParse(url)
-
-    Transcript(url)
 
     # print("Output added to documents.json")
 
@@ -375,7 +246,8 @@ def main():
         
         # Button to trigger the action
         if st.button("Submit"):
-            with st.spinner("Searching..."):
+            with st.echo():
+                st_lottie("https://assets5.lottiefiles.com/packages/lf20_V9t630.json")
                 global finalURL
                 finalURL = ""
                 
