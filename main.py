5851b9feb6ac083414facb8fb552928471c2afc9
Vijay <sccr247@gmail.com>
2023-12-04 19:47:44 -0500

    style

--- main.py
+++ main.py
@@ -315,7 +315,7 @@ def main():
 
     if user_authenticated:
         st.title("GoHere")
-        st.header("Youtube Playlist AI Assistant")
+        st.header("Youtube Playlist AI Search")
 
         global usermsg
            # Create the expander for "How to"
